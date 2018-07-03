a4df7e645eba5eb9f2246a4cd3ee48b1ab0c0749
Yuuki Kojima <yuuki.xx54@gmail.com>
2018-07-03 19:24:19 +0900

    メタタグ機能の追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,33 @@ module ApplicationHelper
   def set_time(time)
     return time.strftime("%Y年%m月%d日")
   end
+
+  def default_meta_tags
+    {
+      site: "Let's Play NovaBlitz",
+      title: '',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'トレーディングカードゲーム「NovaBlitz」の日本人向け情報サイトです。',
+      keywords: 'NovaBlitz, Dapps, TCG',
+      canonical: 'https://lp-novablitz.com/',
+      separator: '|',
+      og: {
+        site_name: "Let's Play NovaBlitz",
+        title: '',
+        description: 'トレーディングカードゲーム「NovaBlitz」の日本人向け情報サイトです。',
+        type: 'website',
+        url: 'https://lp-novablitz.com/',
+        image: image_url('site_image.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary',
+        title: "Let's Play NovaBlitz",
+        site: '@_torike',
+        image: image_url('site_image.png')
+      }
+    }
+  end
+
 end
