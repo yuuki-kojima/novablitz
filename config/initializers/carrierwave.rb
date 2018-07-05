ce6ede642acb0f39374e3afb032e19b3f7011d17
Yuuki Kojima <yuuki.xx54@gmail.com>
2018-07-05 18:49:00 +0900

    秘密鍵関連の編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: '',
-    aws_secret_access_key: '',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
 
